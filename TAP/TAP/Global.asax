7ba94ab88b59c5c9abb5b905d02c7839820da189
unknown <aluno@MAQUINA-03-PC.lti.herois>
2015-05-05 19:11:46 -0300

    enviando projeto MVC e alterando .gitignore

---
+++ TAP/TAP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TAP.MvcApplication" Language="C#" %>
---
+++ TAP/TAP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TAP.Startup))]
+namespace TAP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
