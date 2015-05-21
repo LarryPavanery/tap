af4562bbd464f01496a77bea80c7b26840ad7779
unknown <aluno@MAQUINA-12-PC.lti.herois>
2015-05-20 22:02:17 -0300

    enviando projeto novo

---
+++ NoticiaApp/NoticiaApp/Models/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NoticiaApp.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Noticia> Noticias { get; set; }
+        /// <summary>
+        /// construtor. eh passada a string de conexao, ela devera ser usada para criar a conexao com a base
+        /// ou seja, devera exitir uma string de conexao no arquivo Web.config
+        /// </summary>
+        public DBContext()
+            : base("noticiaapp")
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<DBContext, Configuration>());
+        }
+    }
+}
---
+++ NoticiaApp/NoticiaApp/Models/Noticia.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NoticiaApp.Models
+{
+    public class Noticia
+    {
+        /// <summary>
+        /// id do obj
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// titulo da noticia
+        /// </summary>
+        [Required]
+        public string Titulo { get; set; }
+        /// <summary>
+        /// sub titulo da noticia
+        /// </summary>
+        [Required]
+        public string SubTitulo { get; set; }
+        /// <summary>
+        /// eh o link de quem pertence a noticia, por exemplo, se a noticia vier do olhar digital
+        /// entao essa propriedade tera o link da noticia, referenciando o site deles
+        /// </summary>
+        [Required]
+        public string Meta { get; set; }
+        /// <summary>
+        /// eh o link de quem pertence a noticia, por exemplo, se a noticia vier do olhar digital
+        /// entao essa propriedade tera o link da noticia, referenciando o site deles
+        /// </summary>
+        [Required]
+        [StringLength(300, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 100)]
+        [DataType(DataType.MultilineText)]
+        public string Texto { get; set; }
+        /// <summary>
+        /// data de quando a aplicacao foi criada
+        /// </summary>
+        [Display(Name = "Criação")]
+        public DateTime DataCreated { get; set; }
+
+        /// <summary>
+        /// data da ultima alteracao da noticia, caso alguem precise editar
+        /// </summary>
+        [Display(Name = "Ultima alteração")]
+        public DateTime LastUpdate { get; set; }
+
+        public Noticia()
+        {
+            DataCreated = DateTime.Now;
+            LastUpdate = DateTime.Now;
+        }
+    }
+}
